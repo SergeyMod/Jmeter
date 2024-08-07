604c1df7f8cd1daf5ca256bb72ff37ff0139fe6b
SergeyMod <segaelk@gmail.com>
2024-08-07 21:35:22 +0500

    modified

--- java/HomeWork/src/main/java/HomeWork/model/Kotik.java
+++ java/HomeWork/src/main/java/HomeWork/model/Kotik.java
@@ -4,143 +4,150 @@ import java.util.Random;
 
 public class Kotik {
 
-	private int satiety = 30;
-	private static int count;
-	
-	private String name;
-	private int prettiness;
-	private int weight;
-	private String meow;
-	private String[] eatFood = new String[0];
+    private int satiety = 30;
+    private static int count;
 
-	public Kotik() {
+    private String name;
+    private int prettiness;
+    private int weight;
+    private String meow;
 
-	}
+    public Kotik() {
 
-	public void setKotik(int prettiness, String name, int weight, String meow) {
-		this.prettiness = prettiness;
-		this.name = name;
-		this.weight = weight;
-		this.meow = meow;
-	}
+    }
 
-	public Kotik(int prettiness, String name, int weight, String meow) {
-		setKotik(prettiness, name, weight, meow);
-	}
+    public void setKotik(int prettiness, String name, int weight, String meow) {
+        this.prettiness = prettiness;
+        this.name = name;
+        this.weight = weight;
+        this.meow = meow;
+    }
 
-	{
-		count++;
-	}
+    public Kotik(int prettiness, String name, int weight, String meow) {
+        setKotik(prettiness, name, weight, meow);
+    }
 
-	public static int getCount() {
-		return count;
-	}
+    {
+        count++;
+    }
 
-	public String getName() {
-		return this.name;
-	}
+    public static int getCount() {
+        return count;
+    }
 
-	public String[] getEat() {return this.eatFood;}
+    public String getName() {
+        return this.name;
+    }
 
-	public int getPrettiness() {
-		return this.prettiness;
-	}
+    public int getPrettiness() {
+        return this.prettiness;
+    }
 
-	public String getMeow() {
-		return this.meow;
-	}
+    public String getMeow() {
+        return this.meow;
+    }
 
-	public int getWeight() {
-		return this.weight;
-	}
+    public int getWeight() {
+        return this.weight;
+    }
 
-	public void liveAnotherDay() {
-		Random rnd = new Random();
-		for (int i = 1; i <= 24; i++) {
-			int temp = rnd.nextInt(5);
-			switch(temp) {
-				case 0: if (!play()) {eat();}
-					break;
-				case 1: if (!sleep()) {eat();}
-					break;
-				case 2: if (!chaseMouse()) {eat();}
-					break;
-				case 3: if (!lay()) {eat();}
-					break;
-				case 4: if (!sit()) {eat();}
-			}
-		}
-	}
+    public void liveAnotherDay() {
+        Random rnd = new Random();
+        for (int i = 1; i <= 24; i++) {
+            int temp = rnd.nextInt(5);
+            switch (temp) {
+                case 0:
+                    if (!play()) {
+                        eat();
+                    }
+                    break;
+                case 1:
+                    if (!sleep()) {
+                        eat();
+                    }
+                    break;
+                case 2:
+                    if (!chaseMouse()) {
+                        eat();
+                    }
+                    break;
+                case 3:
+                    if (!lay()) {
+                        eat();
+                    }
+                    break;
+                case 4:
+                    if (!sit()) {
+                        eat();
+                    }
+            }
+        }
+    }
 
-	public boolean play() {
-		boolean _is_eat = is_eat();
-		if (_is_eat) {
-			System.out.println("Кот " + this.name + " играет.");
-			this.satiety -= 1;
-		}
-		return _is_eat;
-	}
-	
-	public boolean sleep() {
-		boolean _is_eat = is_eat();
-		if (_is_eat) {
-			System.out.println("Кот " + this.name + " спит.");
-			this.satiety -= 1;
-		}
-		return _is_eat;
-	}
-	
-	public boolean chaseMouse() {
-		boolean _is_eat = is_eat();
-		if (_is_eat) {
-			System.out.println("Кот " + this.name + " преследует мышь.");
-			this.satiety -= 1;
-		}
-		return _is_eat;
-	}
+    public boolean play() {
+        boolean isEatKot = isEat();
+        if (isEatKot) {
+            System.out.println("Кот " + this.name + " играет.");
+            this.satiety -= 1;
+        }
+        return isEatKot;
+    }
 
-	public boolean sit() {
-		boolean _is_eat = is_eat();
-		if (_is_eat) {
-			System.out.println("Кот " + this.name + " сидит.");
-			this.satiety -= 1;
-		}
-		return _is_eat;
-	}
+    public boolean sleep() {
+        boolean isEatKot = isEat();
+        if (isEatKot) {
+            System.out.println("Кот " + this.name + " спит.");
+            this.satiety -= 1;
+        }
+        return isEatKot;
+    }
 
-	public boolean lay() {
-		boolean _is_eat = is_eat();
-		if (_is_eat) {
-			System.out.println("Кот " + this.name + " лежит.");
-			this.satiety -= 1;
-		}
-		return _is_eat;
-	}
-	
-	public void eat(int eatCount) {
-		this.satiety += eatCount;
-		System.out.println("Кот " + this.name + " поел.");
-	}
+    public boolean chaseMouse() {
+        boolean isEatKot = isEat();
+        if (isEatKot) {
+            System.out.println("Кот " + this.name + " преследует мышь.");
+            this.satiety -= 1;
+        }
+        return isEatKot;
+    }
 
-	public void eat(int eatCount, String foodName) {
+    public boolean sit() {
+        boolean isEatKot = isEat();
+        if (isEatKot) {
+            System.out.println("Кот " + this.name + " сидит.");
+            this.satiety -= 1;
+        }
+        return isEatKot;
+    }
 
-		eat(eatCount);
-		int num = eatFood.length;
-		String[] temp = new String[num + 1];
-        System.arraycopy(this.eatFood, 0, temp, 0, num);
-		temp[num] = foodName;
-		this.eatFood = temp;
-	}
+    public boolean lay() {
+        boolean isEatKot = isEat();
+        if (isEatKot) {
+            System.out.println("Кот " + this.name + " лежит.");
+            this.satiety -= 1;
+        }
+        return isEatKot;
+    }
 
-	public void eat() {
-		eat(10, "Kitekat");
-	}
+    public void eat(int eatCount) {
+        this.satiety += eatCount;
+        System.out.println("Кот " + this.name + " поел.");
+    }
 
-	private boolean is_eat() {
-		if (this.satiety == 0) {
-			System.out.println("Хочу есть, покорми меня");
-			return false;
-		}
-		else return true;
-	}
+    public void eat(int eatCount, String foodName) {
+
+        eat(eatCount);
+        System.out.println("Кот " + this.name + " съел" + foodName);
+    }
+
+    public void eat() {
+        eat(10, "Kitekat");
+    }
+
+    private boolean isEat() {
+        if (this.satiety == 0) {
+            System.out.println("Хочу есть, покорми меня");
+            return false;
+        } else return true;
+    }
 }
