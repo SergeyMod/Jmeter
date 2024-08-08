7e2002b9ab75309d9d04e01872eb330c54fd04dd
SergeyMod <segaelk@gmail.com>
2024-08-08 15:51:00 +0500

    blok

--- java/HomeWork/src/main/java/HomeWork/Application.java
+++ java/HomeWork/src/main/java/HomeWork/Application.java
@@ -9,9 +9,10 @@ public class Application {
 
         Kotik kotik = new Kotik(2, "Рыжик", 2, "мяу");
         Kotik kotik2 = new Kotik();
-        kotik2.setKotik(3, "Kit", 3, "meow");
+        
+	kotik2.setKotik(3, "Kit", 3, "meow");
         kotik2.liveAnotherDay();
-        System.out.println("Имя: " + kotik2.getName() + "\nВес: " + kotik2.getWeight());
+	System.out.println("Имя: " + kotik2.getName() + "\nВес: " + kotik2.getWeight());
         String yesOrNo = (kotik.getMeow().equals(kotik2.getMeow())) ? "" : "не ";
         System.out.println("Коты говорят " + yesOrNo + "одинаково");
         System.out.println("Всего котов: " + Kotik.getCount());
